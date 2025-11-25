9f37f6c3681e78642862098f2edb97942ae2086f
H3urie <125517076+H3urie@users.noreply.github.com>
2025-11-25 18:35:14 -0300

    add ex2.py

---
+++ basico/ex2.py
@@ -0,0 +1,12 @@
+numero_um = int (input("Digite o primeiro numero:"))
+numero_dois = int (input("Digite o segundo numero:"))
+soma = numero_um + numero_dois
+sub = numero_um - numero_dois
+mult = numero_um * numero_dois
+div = numero_um / numero_dois
+
+
+print ("a soma dos dois numeros resulta em:", soma)
+print ("a subtração dos dois numeros resulta em:", sub)
+print ("a multiplicação dos dois numeros resulta em:", mult)
+print ("a divisão dos dois numeros resulta em:", div)
