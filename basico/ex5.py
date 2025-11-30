7446f7f247344327201ecf06e9a9c9ef076fc667
Vinicius Oliveira <125517076+H3urie@users.noreply.github.com>
2025-11-30 20:19:17 -0300

    refactor: code optimization

--- basico/ex5.py
+++ basico/ex5.py
@@ -14,26 +14,20 @@ Se nada for digitado em nome ou idade:
     exiba "Desculpe, você deixou campos vazios."
 """
 
-nome = input("Digite seu nome:")
-idade = input("Digite sua idade:")
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
 
-if not idade or not nome:
+if nome and idade:
+    print(f"seu nome é {nome}")
+    print(f"seu nome invertido é {nome[::-1]}")
+
+    if " " in nome:
+        print("seu nome contém espaços")
+    else:
+        print("seu nome não contém espaços")
+
+    print(f"seu nome tem {len(nome)} letras")
+    print(f"a primeira letra do seu nome é: {nome[0]}")
+    print(f"a última letra do seu nome é: {nome[-1]}")
+else:
     print("Desculpe, você deixou campos vazios.")
-
-elif (" ") in nome:
-    print (f"Seu nome é {nome}")
-    print (f"Seu nome invertido é: {nome[::-1]}")
-    print (f"Seu nome contém espaços")
-    print (f"Seu nome contém: {len(nome)} letras")
-    print (f"A primeira letra do seu nome é: {nome[0:1:]}")
-    print (f"A ultima letra do seu nome é: {nome[-1:len(nome):1]}")
-
-elif (" ") not in nome:
-    print (f"Seu nome é {nome}")
-    print (f"Seu nome invertido é: {nome[::-1]}")
-    print (f"Seu nome não contém espaços")
-    print (f"Seu nome contém: {len(nome)} letras")
-    print (f"A primeira letra do seu nome é: {nome[0:1:]}")
-    print (f"A ultima letra do seu nome é: {nome[-1:len(nome):1]}")
-
-
