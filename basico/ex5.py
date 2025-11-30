5d5382db786209d78ed03d9876d11ea3fb662645
H3urie <125517076+H3urie@users.noreply.github.com>
2025-11-30 19:46:01 -0300

    add text

--- basico/ex5.py
+++ basico/ex5.py
@@ -1,3 +1,19 @@
+"""
+Exercício
+Peça ao usuário para digitar seu nome
+Peça ao usuário para digitar sua idade
+Se nome e idade forem digitados:
+    Exiba:
+        Seu nome é {nome}
+        Seu nome invertido é {nome invertido}
+        Seu nome contém (ou não) espaços
+        Seu nome tem {n} letras
+        A primeira letra do seu nome é {letra}
+        A última letra do seu nome é {letra}
+Se nada for digitado em nome ou idade: 
+    exiba "Desculpe, você deixou campos vazios."
+"""
+
 nome = input("Digite seu nome:")
 idade = input("Digite sua idade:")
 
@@ -19,4 +35,5 @@ elif (" ") not in nome:
     print (f"Seu nome contém: {len(nome)} letras")
     print (f"A primeira letra do seu nome é: {nome[0:1:]}")
     print (f"A ultima letra do seu nome é: {nome[-1:len(nome):1]}")
+
 
